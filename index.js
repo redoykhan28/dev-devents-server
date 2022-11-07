cede7fe81001fe48adc36b4b52e1dd0f34e09154
unknown <redoyk2015@gmail.com>
2022-11-08 05:56:46 +0600

    Mongodb connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -14,8 +15,20 @@ app.get('/', (res, req) => {
     res.send('Devent server is running')
 })
 
+//database
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ytkvvxy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    console.log('connected')
+    client.close();
+});
+
+
 
 app.listen(port, () => {
 
-    console.log(`DEvents runs on port ${port}`)
+    console.log(`Devents runs on port ${port}`)
 })
